82b7b1e38d8bc29a0d1e5dc4db3b3cc709906fed
Claire Delgove <64356038+ClaireDel@users.noreply.github.com>
2022-03-29 12:41:34 +0100

    Update cropping.py

--- 1. Dataset/cropping.py
+++ 1. Dataset/cropping.py
@@ -1,12 +1,10 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
 import argparse
 import csv
 import os
 import shutil
-
 from PIL import Image
 import torch
 import torch.nn.parallel
@@ -19,8 +17,6 @@ import torchvision
 import cv2
 import numpy as np
 import time
-
-
 import _init_paths
 import models
 from config import cfg
